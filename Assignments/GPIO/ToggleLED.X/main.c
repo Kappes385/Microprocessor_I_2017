caca4cf4a7eed7dc899ceb3e5f3122727c359261
Steffen Rahbek Vutborg <svu@ucn.dk>
2017-11-28 19:36:36 +0100

    Added assignment description to ToggleLED assignment

--- Assignments/GPIO/ToggleLED.X/main.c
+++ Assignments/GPIO/ToggleLED.X/main.c
@@ -13,7 +13,15 @@
     #include "solution/solution.h"
 #endif
 /*
- * 
+ *  Assignment:
+ *      Wire-up the circuit shown in the picture in the resources folder
+ *      Write your program so that pushing the button toggles either LD4 or LD5
+ *      once per push
+ *          Note: the button pin should be configured as an input and the pin for
+ *          the LED should be configured as an output.
+ *          You will also need some code to notice when the state of the button
+ *          changes from high to low
+ *      As a second part have the other LED toggle every other push
  */
 int main(int argc, char** argv) {
 
