9142e0e477c81ee3afb82cfd39f74b51a00e698b
Steffen Rahbek Vutborg <svu@ucn.dk>
2017-11-30 08:26:37 +0100

    Solution

---
+++ Assignments/GPIO/ToggleLED.X/solution/solution.c
@@ -0,0 +1,30 @@
+
+#include <xc.h>
+
+#include "solution.h"
+
+void solution()
+{
+    TRISAbits.TRISA3 = 0;   // RA3 -> Output
+    TRISCbits.TRISC1 = 0;   // RC1 -> Output
+    TRISAbits.TRISA2 = 1;   // RA2 -> Input
+    int state = 0;          // This state variable is to keep track of changes of the state of the button on port RA2
+    int state2 = 0;
+    
+    while(1)
+    {
+        if(state != PORTAbits.RA2) 
+        {   // If the state has changed update the state variable and toggle the LED
+            state = PORTAbits.RA2;
+            if (state == 0)
+            {   // if the button is pressed
+                LATAINV = 1<<3; // invert the 4th bit on LATA register (LATA3)
+                state2 = !state2;
+                if (state2 == 0)
+                {
+                    LATCINV = 1<<1; // invert the 2nd bit on LATC register (LATC1)
+                }
+            }
+        }
+    }
+}
---
+++ Assignments/GPIO/ToggleLED.X/solution/solution.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   solution.h
+ * Author: SVU
+ *
+ * Created on 24 November 2017, 10:15
+ */
+
+#ifndef SOLUTION_H
+#define	SOLUTION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void solution();
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SOLUTION_H */
+
