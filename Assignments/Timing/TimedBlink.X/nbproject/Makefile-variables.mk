90a620bdb6ec10f47f7f51de1bcff1340c3af67c
Steffen Rahbek Vutborg <svu@ucn.dk>
2017-12-04 08:59:14 +0100

    fixing git

---
+++ Assignments/Timing/TimedBlink.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TimedBlink.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TimedBlink.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=timedblink.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/timedblink.x.tar
+# solution configuration
+CND_ARTIFACT_DIR_solution=dist/solution/production
+CND_ARTIFACT_NAME_solution=TimedBlink.X.production.hex
+CND_ARTIFACT_PATH_solution=dist/solution/production/TimedBlink.X.production.hex
+CND_PACKAGE_DIR_solution=${CND_DISTDIR}/solution/package
+CND_PACKAGE_NAME_solution=timedblink.x.tar
+CND_PACKAGE_PATH_solution=${CND_DISTDIR}/solution/package/timedblink.x.tar
